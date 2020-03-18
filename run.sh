fffb48260e37aef2b79d472adedb3905068913a4
Morteza Khazamipour <mormoroth@gmail.com>
2020-03-18 18:29:52 +0330

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -e
+
+
 export PROXY_PASS=$PROXY_PASS
 echo $PROXY_PASS | envsubst '$PROXY_PASS' < /etc/nginx/conf.d/default.conf.val > /etc/nginx/conf.d/default.conf
 htpasswd -bBc /etc/nginx/.htpasswd $BASIC_AUTH_USERNAME $BASIC_AUTH_PASSWORD
